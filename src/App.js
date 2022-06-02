241b0a77234e965ef1f1cb908764a11505b74319
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-06-02 12:09:45 +0200

    feat: Added controled forms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { UncontrolledForm } from './components/form';
+import { ControlledForm, UncontrolledForm } from './components/form';
 
 export default function App() {
   return (
     <>
       <h1>Title of the Test Branch</h1>
+      < ControlledForm />
+      <br />
       < UncontrolledForm />
     </>
   );
--- src/components/form.js
+++ src/components/form.js
@@ -1,3 +1,27 @@
+import { useState } from 'react'
+
+
+// Passed a callback function to onChange to save our input value 
+// in our local state. We'll access the typed input value with e.target.value
+function ControlledForm() {
+    const [inputValue, setInputValue] = useState('Ask your question here')
+    const isInputError = inputValue.includes('f')
+    return (
+        <div>
+            {isInputError && (
+                <div>🔥 You are not allowed to use the letter “f” here.</div>
+            )}
+            <textarea
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button onClick={() => alert(inputValue)}>Alert me 🚨</button>
+        </div>
+    )
+}
+
+
+// Functions for uncontrolled forms
 function handleSubmit(e) {
     e.preventDefault()
     alert(e.target['my_input'].value)
@@ -13,4 +37,4 @@ function UncontrolledForm() {
 }
 
 
-export {UncontrolledForm};
+export {ControlledForm, UncontrolledForm};
